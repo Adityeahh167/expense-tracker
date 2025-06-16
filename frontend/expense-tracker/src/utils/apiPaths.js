c33caa93440aaa13ae69a8751a534cce3ff428c5
Adityeahh167 <155294739+Adityeahh167@users.noreply.github.com>
2025-06-16 12:14:05 +0530

    Update apiPaths.js

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://vercel.com/adityas-projects-47719430/expense-tracker-backend";
+export const BASE_URL = "https://expense-tracker-api-lilac.vercel.app/";
 
 export const API_PATHS = {
   AUTH: {

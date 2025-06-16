c25633422f1a0f5851b7aa343d60ca7734827dbf
Adityeahh167 <155294739+Adityeahh167@users.noreply.github.com>
2025-06-16 18:57:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 // ✅ Enable CORS
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.CLIENT_URL||"*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
